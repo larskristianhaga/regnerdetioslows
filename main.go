b679d98b79a76535c4fc9cb6556405573ab34e84
Lars Kristian Haga <larsh@elkjop.no>
2022-10-19 11:47:38 +0200

    Added new endpoint for precipitation

--- main.go
+++ main.go
@@ -15,12 +15,12 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", func(c echo.Context) error {
-		return c.HTML(http.StatusOK, "Hello, Docker! <3")
-	})
-
 	e.GET("/ping", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
+	})
+
+	e.GET("/api/v1/precipitation", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct{ DoesItRain bool; LastUpdated string }{DoesItRain: false, LastUpdated: "A minute ago"})
 	})
 
 	httpPort := os.Getenv("HTTP_PORT")
