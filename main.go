d4bd580210a53736bd7f036274ffe04b082b9c1c
Lars Kristian Haga <larsh@elkjop.no>
2022-10-19 23:23:12 +0200

    Now calls the YR API to get data.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
@@ -15,15 +18,37 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/ping", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, struct{ Status string }{Status: "OK"})
-	})
-
 	e.GET("/api/v1/precipitation", func(c echo.Context) error {
+
+		// API endpoint.
+		endpoint := "https://www.yr.no/api/v0/locations/1-72837/forecast/now"
+
+		// Get data from the API.
+		response, err := http.Get(endpoint)
+
+		// Guard for empty response.
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		// Get the JSON data out of the response.
+		responseData, err := ioutil.ReadAll(response.Body)
+
+		// Guard for something wrong with reading the content.
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		var yr Yr
+		_ = json.Unmarshal(responseData, &yr)
+
+		var doesItRain = yr.Points[1].Precipitation.Intensity > 0
+		var lastUpdatedAt = yr.Points[1].Time
+
 		return c.JSON(http.StatusOK, struct {
-			DoesItRain  bool
-			LastUpdated string
-		}{DoesItRain: false, LastUpdated: "A minute ago"})
+			DoesItRain   bool
+			DataFromTime string
+		}{DoesItRain: doesItRain, DataFromTime: lastUpdatedAt})
 	})
 
 	httpPort := os.Getenv("HTTP_PORT")
@@ -31,5 +56,5 @@ func main() {
 		httpPort = "8080"
 	}
 
-	e.Logger.Fatal(e.Start(":" + httpPort))
+	log.Fatal(e.Start(":" + httpPort))
 }
